e1f66f245f54ec5a7a1057fd31b46ce4b1c3a296
Rafael Oliveira <r0liveira@icloud.com>
2026-07-05 00:46:21 -0400

    EXWM: media keys fix, sk commands

--- emacs/exwm.el
+++ emacs/exwm.el
@@ -1,4 +1,5 @@
 (require 'exwm)
+(require 'subr-x)
 
 (defvar sk/wallpaper-file
   (expand-file-name "~/Projects/guix-dotfiles/assets/wallpapers/sky.png"))
@@ -10,27 +11,46 @@
     (start-process "xwallpaper" nil
                    "xwallpaper" "--zoom" sk/wallpaper-file)))
 
-(exwm-input-set-key (kbd "s-<return>")
-                    (lambda ()
-                      (interactive)
-                      (start-process-shell-command "kitty" nil "kitty")))
+(defun sk/exwm-launch-kitty ()
+  (interactive)
+  (start-process-shell-command "kitty" nil "kitty")
+  (message "Launching kitty"))
 
-(exwm-input-set-key (kbd "s-w")
-                    (lambda ()
-                      (interactive)
-                      (start-process-shell-command
-                       "browser" nil "chromium")))
+(defun sk/exwm-launch-browser ()
+  (interactive)
+  (start-process-shell-command "browser" nil "chromium")
+  (message "Launching chromium"))
+
+(defun sk/exwm-reload ()
+  (interactive)
+  (load-file "~/.emacs.d/exwm.el")
+  (message "EXWM config reloaded"))
+
+(defun sk/exwm-update-title ()
+  (exwm-workspace-rename-buffer
+   (string-trim
+    (format "%s%s%s"
+            (or exwm-class-name "EXWM")
+            (if (and exwm-title (not (string-empty-p exwm-title))) ": " "")
+            (or exwm-title "")))))
+
+(add-hook 'exwm-update-class-hook #'sk/exwm-update-title)
+(add-hook 'exwm-update-title-hook #'sk/exwm-update-title)
+
+(exwm-input-set-key (kbd "s-<return>") #'sk/exwm-launch-kitty)
+(exwm-input-set-key (kbd "s-w") #'sk/exwm-launch-browser)
+(exwm-input-set-key (kbd "s-r") #'sk/exwm-reload)
 
 (defun sk/volume-raise ()
   (interactive)
   (start-process "wpctl-volume-up" nil
-		 "wpctl" "set volume" "-l" "1.0"
+		 "wpctl" "set-volume" "-l" "1.0"
 		 "@DEFAULT_AUDIO_SINK@" "5%+"))
 
 (defun sk/volume-lower ()
   (interactive)
   (start-process "wpctl-volume-down" nil
-		 "wpctl" "set volume"
+		 "wpctl" "set-volume"
 		 "@DEFAULT_AUDIO_SINK@" "5%-"))
 
 (exwm-input-set-key (kbd "<XF86AudioRaiseVolume>") #'sk/volume-raise)
