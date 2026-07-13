62e2849bfbea2b2ebf5b4ff10adb6228b84fe5e0
skydive420dz <r0liveira@icloud.com>
2026-07-12 21:15:00 -0400

    guix: add authenticated promotion gates

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,6 +3,7 @@
   (name 'guix)
   (url "https://git.guix.gnu.org/guix.git")
   (branch "master")
+  (commit "a8391f2d7451c2463ba253ffa9872fa6f27485d7")
   (introduction
    (make-channel-introduction
     "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -11,6 +12,8 @@
  (channel
   (name 'nonguix)
   (url "https://gitlab.com/nonguix/nonguix")
+  (branch "master")
+  (commit "3b66965566fe8c96edb5a41fd39a9e5a90ad9b61")
   (introduction
    (make-channel-introduction
     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -19,4 +22,10 @@
  (channel
   (name 'sk-guix)
   (url "https://github.com/skydive420dz/sk-guix.git")
-  (branch "main")))
+  (branch "main")
+  (commit "b84ff24a515b2b6d0db85c0da30960c96e20ad22")
+  (introduction
+   (make-channel-introduction
+    "eaade3680892ce74ebae68f4922cd0eb4a463a17"
+    (openpgp-fingerprint
+     "6B09 4D15 B02E 54B0 F6B5  E9E8 6F83 FC62 D232 E5EC")))))
